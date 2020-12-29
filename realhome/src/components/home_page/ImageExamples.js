71b1c9cea10250a716923d4debb508595771700d
Dylan Adams <dylancadams1@gmail.com>
2020-12-29 12:52:01 -0500

    Update ImageExamples.js

--- realhome/src/components/home_page/ImageExamples.js
+++ realhome/src/components/home_page/ImageExamples.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image1 from '../../images/image1.jpg'
+
 import Image2 from '../../images/image2.jpg'
 import Image3 from '../../images/image3.jpg'
 import Image4 from '../../images/image4.jpg'
@@ -44,11 +44,11 @@ function ImageExample ()
                     
                     <Tab className="pad" eventKey="Interiors" title="Interiors" fluid>
                     
-                        <NewRow src={Image1} src1={Image2} />
+                        <NewRow src={Image2} src1={Image2} />
 
-                        <NewRow src={Image1} src1={Image2} />
+                        <NewRow src={Image2} src1={Image2} />
 
-                        <NewRow src={Image1} src1={Image2} />
+                        <NewRow src={Image2} src1={Image2} />
                     
                     </Tab>
 
