15221e738a2e643626dc394544c355d7326a5063
Dylan Adams <dylancadams1@gmail.com>
2023-08-14 18:42:44 -0400

    Improved footer responsiveness

--- realhome/src/components/home_page/Footer.js
+++ realhome/src/components/home_page/Footer.js
@@ -12,31 +12,34 @@ function Footer()
     return (
         <div className={styles.footerdiv}>
             <Container fluid>
-                <Row style={{paddingTop: 0}}>
-                    <Col>
+                <Row className={styles.row}>
+                    <Col className={styles.col}>
                         <Image src={logo} className={styles.logo} alt="Real Home Photography" fluid />
                     </Col>
 
-                    <Col className="col-sm-2">
+                    <Col className={styles.col}>
                         <Image src={veteran} className={styles.vet} alt="Real Home Photography"  />
                     </Col>
 
-                    <Col className={styles.zillowcol}>
-                        <Image src={zillow} className={`${styles.vet} ${styles.zillow}`} alt="Real Home Photography Zillow"  />
+                    <Col className={styles.col}>
+                        <Image src={zillow} className={styles.zillow} alt="Real Home Photography Zillow"  />
                     </Col>
 
-                    <Col className={`${styles.contactcol} col-sm-2`} style={{paddingRight: 0}}>
+                    <Col className={styles.col}>
                         <header className={styles.contactinfo}>
                             Phone : <a href="tel:9196791940" aria-label="Real Home Photography Phone Number" rel="noopener"> 919-679-1940 </a> 
                         </header>
                     </Col>
 
-                    <Col style={{paddingLeft:0}} >
+                    <Col className={styles.col} >
                         <a href="https://www.instagram.com/rhomephotography/" target="_blank" aria-label="Instagram" rel="noreferrer">
-                            <Image src={insta}  className={styles.socials} alt="Instagram" fluid />
+                            <Image src={insta} alt="Instagram" fluid />
                         </a>
+                    </Col>
+
+                    <Col className={styles.col}>
                         <a href="https://www.facebook.com/RealHomePhotography/" target="_blank" aria-label="Facebook" rel="noreferrer">
-                            <Image src={facebook} className={styles.socials} alt="Facebook" fluid />
+                            <Image src={facebook} alt="Facebook" fluid />
                         </a>
                     </Col>
                 
